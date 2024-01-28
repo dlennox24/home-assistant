4eb302d64b2db3e79c3909a80d8c0d53ab6b7d61
Daniel Lennox <dlennox2424@gmail.com>
2024-01-28 10:30:25 -0700

    Updating backup script to loop over multiple containers

--- home-assistant/backup.sh
+++ home-assistant/backup.sh
@@ -1,31 +1,65 @@
 #!/bin/bash
-readonly SOURCE_DIR="/opt/home-assistant"
-readonly BACKUP_DIR="/mnt/backup/home-assistant"
-readonly BACKUP_PATH="${BACKUP_DIR}/$(date '+%F')"
-readonly LATEST_LINK="${BACKUP_DIR}/latest"
+readonly containers='[
+  {
+    "dockerName": "home-assistant",
+    "srcDir": "/opt/home-assistant",
+    "backupDir": "/mnt/backup/home-assistant"
+  },
+  {
+    "dockerName": "esphome",
+    "srcDir": "/opt/esphome",
+    "backupDir": "/mnt/backup/esphome"
+  }
+]'
+readonly len=$(echo "$containers" | grep -o '{' | wc -l)
 
-if [ ! -d "${BACKUP_DIR}" ]; then
-  echo "${BACKUP_DIR} does not exist. Creating ${BACKUP_DIR}"
-  mkdir -p "${BACKUP_DIR}"
-fi
+for ((i = 0; i < len; i++)); do
 
-docker stop home-assistant
+  DOCKER_CONTAINER=$(echo "$containers" | grep -oP '"dockerName": "\K[^"]+' | sed -n $((i+1))p)
+  SOURCE_DIR=$(echo "$containers" | grep -oP '"srcDir": "\K[^"]+' | sed -n $((i+1))p)
+  BACKUP_DIR=$(echo "$containers" | grep -oP '"backupDir": "\K[^"]+' | sed -n $((i+1))p)
+  BACKUP_PATH="${BACKUP_DIR}/$(date '+%F')"
+  LATEST_LINK="${BACKUP_DIR}/latest"
 
-rsync -a --delete \
-  "${SOURCE_DIR}/" \
-  --link-dest "${LATEST_LINK}" \
-  "${BACKUP_PATH}"
+  echo "Backup starting for ${DOCKER_CONTAINER}"
+  echo ''
 
-cd "${BACKUP_PATH}"
-tar -czf "${BACKUP_PATH}.tar.gz" .
-cd -
+  echo "${DOCKER_CONTAINER}"
+  echo "${SOURCE_DIR}"
+  echo "${BACKUP_DIR}"
+  echo "${BACKUP_PATH}"
+  echo "${LATEST_LINK}"
 
-rm -rf "${BACKUP_PATH}"
+  if [ ! -d "${BACKUP_DIR}" ]; then
+    echo "Creating ${BACKUP_DIR}"
+    mkdir -p "${BACKUP_DIR}"
+  fi
 
-if [ -d "${LATEST_LINK}" ]; then
-  rm -rf "${LATEST_LINK}"
-fi
+  docker stop "${DOCKER_CONTAINER}"
 
-ln -s "${BACKUP_PATH}.tar.gz" "${LATEST_LINK}"
+  rsync -a --delete \
+    "${SOURCE_DIR}/" \
+    --link-dest "${LATEST_LINK}" \
+    "${BACKUP_PATH}"
 
-docker start home-assistant
+  cd "${BACKUP_PATH}"
+  tar -czf "${BACKUP_PATH}.tar.gz" .
+  cd -
+
+  rm -rf "${BACKUP_PATH}"
+
+  if [ -d "${LATEST_LINK}" ]; then
+    rm -rf "${LATEST_LINK}"
+  fi
+
+  ln -s "${BACKUP_PATH}.tar.gz" "${LATEST_LINK}"
+
+  docker start "${DOCKER_CONTAINER}"
+
+  if [ ! $i -eq $((len-1)) ]; then
+    echo ''
+    echo '===================================='
+    echo ''
+  fi
+
+done
